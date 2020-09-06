00c8c0c739f8939e466f7c45e00d339ff7039f1a
Chris Sanchez <c.sanch7@gmail.com>
2020-09-06 12:07:56 -0700

    add connectDB()

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log('Connected to MongoDB. . .');
+  } catch (err) {
+    console.error(err.message);
+
+    process.exit(1); // Exit process with failure
+  }
+}
+
+
+module.exports = connectDB;
