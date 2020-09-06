38fef6de0ba480bda1ae7caa98a1c306d67a52c6
Chris Sanchez <c.sanch7@gmail.com>
2020-09-06 11:55:10 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('API RUNNING'));
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server up and running on port ${PORT}`));
