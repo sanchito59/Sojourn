36c5e98f1118d4cf549db9c1e5fa438da3e5bc94
Chris Sanchez <c.sanch7@gmail.com>
2020-09-06 14:02:44 -0700

    create middleware to verify auth'd jsonwebtoken

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function (req, res, next) {
+  const token = req.header('x-auth-token'); // Get token
+
+  if (!token) {
+    return res.status(401).json({ msg: 'Not authorized' }); // Check token
+  }
+
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret')); // Verify token
+
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is invalid' });
+  }
+}
+
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route GET   api/auth
+const User = require('../../models/User');
+
+// @route       GET api/auth
 // @desc        Test Route
 // @access      Public
-router.get('/', (req, res) => res.send('Auth router'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
 
 module.exports = router;
