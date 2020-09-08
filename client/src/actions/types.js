a3835466a4876aaaffdca637193a4448c59a512e
Chris Sanchez <c.sanch7@gmail.com>
2020-09-07 22:17:41 -0700

    add functionality to GET Profile

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -9,3 +9,6 @@ export const AUTH_ERROR = 'AUTH_ERROR';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const LOGOUT = 'LOGOUT';
+// PROFILE
+export const GET_PROFILE = 'GET_PROFILE';
+export const PROFILE_ERROR = 'PROFILE_ERROR';
