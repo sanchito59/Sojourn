3347193a759e16eed024e8b44944c0dfcc7df5cc
Chris Sanchez <c.sanch7@gmail.com>
2020-09-12 13:25:53 -0700

    delete Account

--- client/src/components/pages/Dashboard/index.jsx
+++ client/src/components/pages/Dashboard/index.jsx
@@ -1,15 +1,17 @@
 import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { getCurrentProfile } from "../../../actions/profile";
 import { Button, CircularProgress, Typography } from "@material-ui/core";
+import { DeleteForever } from "@material-ui/icons";
 import DashboardActions from "../../molecules/DashboardActions";
 import CustomLink from "../../atoms/CustomLink";
 import ExperienceList from "../../molecules/ExperienceList";
 import EducationList from "../../molecules/EducationList";
+import { getCurrentProfile, deleteAccount } from "../../../actions/profile";
 
 const Dashboard = ({
   getCurrentProfile,
+  deleteAccount,
   auth: { user },
   profile: { profile, loading },
 }) => {
@@ -32,6 +34,14 @@ const Dashboard = ({
           <DashboardActions />
           <ExperienceList experience={profile.experience} />
           <EducationList education={profile.education} />
+          <Button
+            variant="contained"
+            color="secondary"
+            startIcon={<DeleteForever />}
+            onClick={() => deleteAccount()}
+          >
+            DELETE ACCOUNT
+          </Button>
         </>
       ) : (
         <>
@@ -56,6 +66,7 @@ const Dashboard = ({
 
 Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
+  deleteAccount: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
   profile: PropTypes.object.isRequired,
 };
@@ -65,4 +76,6 @@ const mapStateToProps = (state) => ({
   profile: state.profile,
 });
 
-export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
+export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(
+  Dashboard
+);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,7 @@ const { check, validationResult } = require('express-validator')
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 // @route       GET api/profile/me
 // @desc        Get current user's profile
@@ -130,6 +131,10 @@ router.get('/user/:user_id', async (req, res) => {
 // @access      Private
 router.delete('/', auth, async (req, res) => {
   try {
+    // DELETE USER'S POSTS
+    await Post.deleteMany({ user: req.user.id });
+
+    // DELETE USER'S PROFILE/USER
     await Profile.findOneAndRemove({ user: req.user.id });
 
     await User.findOneAndRemove({ _id: req.user.id });
