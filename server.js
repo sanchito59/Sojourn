b2b30d7b3cb329d41242473cc0097a66f53a713b
Chris Sanchez <c.sanch7@gmail.com>
2020-09-19 01:13:11 -0700

    add folder for production variables, fix server.js for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,13 +10,20 @@ connectDB();
 // Initialize Middleware
 app.use(express.json({ extended: false, }));
 
-app.get('/', (req, res) => res.send('API RUNNING'));
-
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build')); // from Heroku postbuild script
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+};
 
 const PORT = process.env.PORT || 5000
 
